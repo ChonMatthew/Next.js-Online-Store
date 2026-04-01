c1050d938c8d24bb02cf5e6a47c329af8a57da1e
Your Name <you@example.com>
2026-04-01 18:26:52 +0800

    pages setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,6 @@ const Home = () => {
   return (
     <Container className="">
       <h2 className="text-xl font-semibold">Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        tempora dolor vitae, dolore incidunt dolorem, debitis fugit saepe
-        repudiandae at quaerat harum veritatis suscipit, eius perspiciatis
-        deserunt ipsa non excepturi reiciendis impedit perferendis quidem sit?
-        Omnis nulla magni eius sunt accusantium, fuga, fugit quo excepturi
-        tempore laboriosam reiciendis sequi illo.
-      </p>
     </Container>
   );
 };
